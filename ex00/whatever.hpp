286123dc9ac14374b213653db07d4191048adef6
agengemb <gengembre.alexis@gmail.com>
2023-12-21 17:21:14 +0100

    ex01 fini

--- ex00/whatever.hpp
+++ ex00/whatever.hpp
@@ -1,20 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   iter.hpp                                           :+:      :+:    :+:   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: agengemb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/21 16:18:28 by agengemb          #+#    #+#             */
-/*   Updated: 2023/12/21 16:30:11 by agengemb         ###   ########.fr       */
+/*   Created: 2023/12/21 16:04:21 by agengemb          #+#    #+#             */
+/*   Updated: 2023/12/21 16:23:38 by agengemb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-template<typename T>
-
-void	iter(T* array_ptr, std::size_t length, )
+template <typename T>
+void swap(T& a, T& b)
 {
+	T temp;
+	temp = a;
+	a = b;
+	b = temp;
+}
 
+template <typename T>
+const T& min(const T& a, const T& b)
+{
+	if (a < b)
+		return (a);
+	return (b);
+}
+
+template <typename T>
+const T& max(const T& a, const T& b)
+{
+	if (a > b)
+		return (a);
+	return (b);
 }
