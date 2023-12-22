f20b499f95c53a934168b194ab54343f2654ff2d
agengemb <gengembre.alexis@gmail.com>
2023-12-22 18:03:34 +0100

    vendredi boulot

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -6,15 +6,16 @@
 /*   By: agengemb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/21 16:18:28 by agengemb          #+#    #+#             */
-/*   Updated: 2023/12/21 16:30:11 by agengemb         ###   ########.fr       */
+/*   Updated: 2023/12/22 16:12:23 by agengemb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-template<typename T>
+template<typename T, typename J>
 
-void	iter(T* array_ptr, std::size_t length, )
+void	iter(T* array_ptr, std::size_t length, J function)
 {
-
+	for (std::size_t i = 0; i < length; ++i)
+		function(array_ptr[i]);
 }
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,8 +6,29 @@
 /*   By: agengemb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/21 16:18:22 by agengemb          #+#    #+#             */
-/*   Updated: 2023/12/21 16:18:23 by agengemb         ###   ########.fr       */
+/*   Updated: 2023/12/22 16:35:46 by agengemb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "iter.hpp"
 
+void mult(int v)
+{
+	std::cout << 3 * v << std::endl;
+}
+
+void mult_affect(int& v)
+{
+	v *= 3;
+}
+
+int main(void)
+{
+	int array[3] = {4, 5, 3};
+	
+	iter(array, 3, mult);	
+	iter(array, 3, mult_affect);
+	for (int i = 0; i < 3; ++i)
+		std::cout << array[i] << std::endl;	
+	return (0);
+}
